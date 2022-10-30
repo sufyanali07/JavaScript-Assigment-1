5ba34e99797b909698bc8a1369161eea0d3654a4
sufyanali07 <sufyan.ali6800@gmail.com>
2022-10-30 13:24:11 -0700

    Added New Files

---
+++ app.js
@@ -0,0 +1,120 @@
+1.//=====================
+
+
+
+//alert("Hello JavaScript");
+//window.alert("Hello J^S");
+//var a = "Hello JavaScript";
+//alert(a);
+
+
+
+2.//=====================
+
+
+
+//var b = 10;
+//alert(b);
+
+
+
+3.//=====================
+
+
+
+//var a = 10;
+//var b = 20;
+//var c = a + b;
+//var c = b - a;
+//var c = b * a;
+//var c = (a + b) * a;
+//var c = a + b * a;
+//var c = b / a;
+//alert(c)
+
+
+
+4.///=====================
+
+
+
+// var a;
+
+// a = "Sufyann";
+
+// alert(a);
+
+// a = "ABC";
+
+// alert(a);
+
+
+
+5.//=========================
+
+
+// var a = 10;
+
+// var b = "20";
+
+// var c = b + a;
+
+// alert(c)
+
+
+6.//=========================
+
+
+//var abc //legal
+
+//var 123 //illegal
+
+//var ab12 //legal
+
+//var 12ab //illegal
+
+//var alert //illegal
+
+//var @abc //illegal
+
+//var abc@ //illegal
+
+//var _abc //legal
+
+//var -abc //illegal
+
+//var a-b //illegal
+
+
+7.//=======================
+
+
+// var a = 10;
+
+// var b = 20;
+
+// a++;
+// b--;
+
+// console.log(a);
+// console.log(b);
+
+8.//======================
+
+// var a = 10;
+
+// var b = a++;
+
+// console.log(a);
+
+// console.log(b);
+
+.9//=======================
+
+
+// var c = a++ - --b + a - b + b - --a - ++a + --b;
+//      -19 = 18
+//      -1
+
+
+//=======================
